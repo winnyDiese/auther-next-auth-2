c82285e91fd5e943b8ce7d05965dc8a8a5fcbbdb
winnyDiese <winnydiese@gmail.com>
2023-12-17 22:14:22 +0100

    Configurate (credentials and options of nextAuth)

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,20 @@
 
-import NextAuth from "next-auth"
+import NextAuth, {NextAuthOptions} from "next-auth"
 import { NextResponse } from "next/server"
 
+const authOptions = {
+    providers:[
+        CredentialsProvider({
+            name: "creds",
+            credentials: {
+                email: {label:"Email", placeholder:"Enter email..."},
+                password: {label:"Password", placeholder:"Enter password..."}
+            }, 
+            async authorize(credentials, req){
+
+            }
+        })
+    ]
+}
 const handler = NextAuth()
 export { handler as GET, handler as POST}
