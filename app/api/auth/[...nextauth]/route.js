81edd2c3256e21379e1f6ec49b621469d79dfb7b
winnyDiese <winnydiese@gmail.com>
2023-12-17 22:31:04 +0100

    Finish to configurate credentials of next-auth options

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,11 +10,14 @@ const authOptions = {
                 email: {label:"Email", placeholder:"Enter email..."},
                 password: {label:"Password", placeholder:"Enter password..."}
             }, 
-            async authorize(credentials, req){
-
+            async authorize(credentials){
+                if(!credentials || !credentials.email || !credentials.password) return null
+                const user = users.find(item => item.email === credentials.email)
+                if(user?.password === credentials.password) return null
+                return null
             }
         })
     ]
 }
-const handler = NextAuth()
+const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST}
