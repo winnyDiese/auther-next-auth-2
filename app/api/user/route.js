d0468b76c63cee5cd3ebbe834ea1b7cb162f9502
winnyDiese <winnydiese@gmail.com>
2023-12-18 10:47:40 +0100

    Creating api user from mongodb.atlas (cloud on line

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,8 +1,16 @@
-import { users } from "@/app/helpers/constants"
+// import { users } from "@/app/helpers/constants"
+import user from "@/models/User"
+import { connect } from "mongoose"
 import  { NextResponse } from "next/server"
 
 export async function GET(){
-    
-    const users = await users.find()
-    return NextResponse.json({ message:"The users !", users })
+    await connect(process.env.MONGODB_URL)
+    const users = await user.find()
+    return NextResponse.json({ message:"The users !", users },{status:201})
+}
+
+export async function POST(request){
+    const {name,email,password} = await request.json()
+    await user.create({name,email,password})
+    return NextResponse.json({ message:"User created !" },{status:200})
 }
